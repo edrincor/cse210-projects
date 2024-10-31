37c491ede63a07b976301d2cd2551bab00871e93
edrincor <corbynedrington@gmail.com>
2024-10-31 09:35:18 -0600

    Completed C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        Console.Write("What is your grade? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60) 
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (grade % 10 >= 7 && 60 <= grade && grade <= 90)
+        {
+            letter += "+";
+        }
+        else if (grade % 10 <= 3 && 60 <= grade && grade <= 93)
+        {
+            letter += "-";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}");
+
+        if (grade >= 70)
+        {
+            Console.Write("Congratulations, you passed the class!");
+        }
+        else
+        {
+            Console.Write("You didn't pass, better luck next time!");
+        }
     }
 }
