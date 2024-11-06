c2c9787355c29c818bb034a6c3760a8adf191c51
edrincor <corbynedrington@gmail.com>
2024-11-06 10:02:06 -0700

    Completed Learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Instantiating Job objects
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Instantiating Resume object
+        Resume resume1 = new Resume();
+        resume1._name = "Allison Rose";
+        resume1._jobs = [job1, job2];
+
+        // Calling Display functions
+        resume1.Display();
     }
 }
